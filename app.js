b4e21fea571424ac4f6cb736fcd2f12d7be16595
Wali Shah <walishahllc@gmail.com>
2024-06-21 15:06:15 +0500

    Slice

---
+++ app.js
@@ -0,0 +1,8 @@
+let laptop = "Lenovo";
+
+// let newlaptop = laptop.slice(1);          //enovo
+// let newlaptop = laptop.slice(1, 3);       //en
+let newlaptop = laptop.slice(-2);            //vo
+
+
+console.log(newlaptop);
